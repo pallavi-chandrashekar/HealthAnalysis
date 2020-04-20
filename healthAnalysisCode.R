7b601e010ef8ac30a9220bc328d7f0b6fe8ae88c
Mayank Phadke <mphadke1@hawk.iit.edu>
2020-04-20 00:37:40 -0500

    added cleaning code for 2016 dataset

--- healthAnalysisCode.R
+++ healthAnalysisCode.R
@@ -12,7 +12,7 @@ library(randomForest)
 }
 
 cleanDataSet2014 <- function(){
-  survey <- read.csv("C:/Users/i513930/Desktop/My/CSP571/Project/survey.csv",stringsAsFactors = F)
+  survey <- read.csv("C:/Users/Mayank Phadke/Documents/College/DPA/HealthAnalysis/survey.csv",stringsAsFactors = F)
   
   
   #Correcting the spelling of Gender and replace the negative and values more than 65 with median of age
@@ -50,10 +50,134 @@ cleanDataSet2014 <- function(){
   new_survey$state<-NULL
   
   #write the cleaned data to csv
-  write.csv(new_survey, "C:/Users/i513930/Desktop/My/CSP571/Project/new_survey.csv", row.names = F)
+  write.csv(new_survey, "C:/Users/Mayank Phadke/Documents/College/DPA/HealthAnalysis/new_survey.csv", row.names = F)
   return(new_survey)
 }
 
+cleanDataSet2016 <- function(){
+  # read the csv
+  survey <- read.csv(
+    "C:/Users/Mayank Phadke/Documents/College/DPA/HealthAnalysis/survey2016.csv",
+    stringsAsFactors = F)
+
+  col_names <- c(
+    "Age",
+    "Gender",
+    "Country",
+    "state",
+    "self_employed",
+    "family_history",
+    "treatment",
+    "no_employees",
+    "remote_work",
+    "tech_company",
+    "benefits",
+    "care_options",
+    "wellness_program",
+    "seek_help",
+    "anonymity",
+    "leave",
+    "mental_health_consequence",
+    "phys_health_consequence",
+    "coworkers",
+    "supervisor",
+    "mental_health_interview",
+    "phys_health_interview",
+    "mental_vs_physical",
+    "obs_consequence"
+  )
+
+  # renaming column names
+  names(survey)[names(survey) == "What.is.your.age."] <- "Age"
+  names(survey)[names(survey) == "What.is.your.gender."] <- "Gender"
+  names(survey)[names(survey) == "What.country.do.you.live.in."] <- "Country"
+  names(survey)[names(survey) == "What.US.state.or.territory.do.you.live.in."] <- "state"
+  names(survey)[names(survey) == "Are.you.self.employed."] <- "self_employed"
+  names(survey)[names(survey) == "Do.you.have.a.family.history.of.mental.illness."] <- "family_history"
+  names(survey)[names(survey) == "Have.you.ever.sought.treatment.for.a.mental.health.issue.from.a.mental.health.professional."] <- "treatment"
+  names(survey)[names(survey) == "If.you.have.a.mental.health.issue..do.you.feel.that.it.interferes.with.your.work.when.being.treated.effectively."] <- "work_interfere_effective"
+  names(survey)[names(survey) == "If.you.have.a.mental.health.issue..do.you.feel.that.it.interferes.with.your.work.when.NOT.being.treated.effectively."] <- "work_interfere_not_effective"
+  names(survey)[names(survey) == "How.many.employees.does.your.company.or.organization.have."] <- "no_employees"
+  names(survey)[names(survey) == "Do.you.work.remotely."] <- "remote_work"
+  names(survey)[names(survey) == "Is.your.employer.primarily.a.tech.company.organization."] <- "tech_company"
+  names(survey)[names(survey) == "Does.your.employer.provide.mental.health.benefits.as.part.of.healthcare.coverage."] <- "benefits"
+  names(survey)[names(survey) == "Do.you.know.the.options.for.mental.health.care.available.under.your.employer.provided.coverage."] <- "care_options"
+  names(survey)[names(survey) == "Has.your.employer.ever.formally.discussed.mental.health..for.example..as.part.of.a.wellness.campaign.or.other.official.communication.."] <- "wellness_program"
+  names(survey)[names(survey) == "Does.your.employer.offer.resources.to.learn.more.about.mental.health.concerns.and.options.for.seeking.help."] <- "seek_help"
+  names(survey)[names(survey) == "Is.your.anonymity.protected.if.you.choose.to.take.advantage.of.mental.health.or.substance.abuse.treatment.resources.provided.by.your.employer."] <- "anonymity"
+  names(survey)[names(survey) == "If.a.mental.health.issue.prompted.you.to.request.a.medical.leave.from.work..asking.for.that.leave.would.be."] <- "leave"
+  names(survey)[names(survey) == "Do.you.think.that.discussing.a.mental.health.disorder.with.your.employer.would.have.negative.consequences."] <- "mental_health_consequence"
+  names(survey)[names(survey) == "Do.you.think.that.discussing.a.physical.health.issue.with.your.employer.would.have.negative.consequences."] <- "phys_health_consequence"
+  names(survey)[names(survey) == "Would.you.feel.comfortable.discussing.a.mental.health.disorder.with.your.coworkers."] <- "coworkers"
+  names(survey)[names(survey) == "Would.you.feel.comfortable.discussing.a.mental.health.disorder.with.your.direct.supervisor.s.."] <- "supervisor"
+  names(survey)[names(survey) == "Would.you.bring.up.a.mental.health.issue.with.a.potential.employer.in.an.interview."] <- "mental_health_interview"
+  names(survey)[names(survey) == "Would.you.be.willing.to.bring.up.a.physical.health.issue.with.a.potential.employer.in.an.interview."] <- "phys_health_interview"
+  names(survey)[names(survey) == "Do.you.feel.that.your.employer.takes.mental.health.as.seriously.as.physical.health."] <- "mental_vs_physical"
+  names(survey)[names(survey) == "Have.you.heard.of.or.observed.negative.consequences.for.co.workers.who.have.been.open.about.mental.health.issues.in.your.workplace."] <- "obs_consequence"
+
+  new_survey <- survey[, col_names]
+
+  # We are assuming all employees are between the age of 18 and 75
+  mean_age <- round(mean(filter(new_survey, (Age >= 18 & Age <= 65))$Age))
+
+  # Replace values with mean age
+  new_survey$Age[new_survey$Age < 18 | new_survey$Age > 65] <- mean_age
+
+  # Formatting Gender
+  new_survey$Gender <- trimws(toupper(new_survey$Gender))
+
+  male <- c("M", "MALE", "MALE.", "MALE (CIS)", "MAN", "CIS MALE", "SEX IS MALE", "MALR", "DUDE", "I'M A MAN WHY DIDN'T YOU MAKE THIS A DROP DOWN QUESTION. YOU SHOULD OF ASKED SEX? AND I WOULD OF ANSWERED YES PLEASE. SERIOUSLY HOW MUCH TEXT CAN THIS TAKE?", "MAIL", "M|", "MALE/GENDERQUEER", "CISDUDE", "CIS MAN")
+  female <- c("F", "FEMALE", "I IDENTIFY AS FEMALE.", "FEMALE ASSIGNED AT BIRTH", "WOMAN", "FM", "CIS FEMALE", "FEMALE OR MULTI-GENDER FEMME", "FEMALE/WOMAN", "CISGENDER FEMALE", "FEM", "CIS-WOMAN", "FEMALE (PROPS FOR MAKING THIS A FREEFORM FIELD, THOUGH)", "FEMALE-BODIED; NO FEELINGS ABOUT GENDER")
+  other <- c("BIGENDER", "NON-BINARY", "TRANSITIONED, M2F", "GENDERFLUID (BORN FEMALE)", "ANDROGYNOUS", "MALE 9:1 FEMALE, ROUGHLY", "N/A", "OTHER", "NB MASCULINE", "NONE OF YOUR BUSINESS", "GENDERQUEER", "OTHER/TRANSFEMININE", "HUMAN", "GENDERFLUID", "ENBY", "GENDERQUEER WOMAN", "MTF", "QUEER", "AGENDER", "FLUID", "NONBINARY", "UNICORN", "MALE (TRANS, FTM)", "GENDERFLUX DEMI-GIRL", "", "AFAB", "TRANSGENDER WOMAN")
+
+  for(i in 1:nrow(survey)){
+    if(new_survey$Gender[i] %in% male){
+      new_survey$Gender[i]<- 'M'
+    }
+    if(new_survey$Gender[i] %in% female == TRUE){
+      new_survey$Gender[i]<- 'F'
+    }
+    if(new_survey$Gender[i] %in% other == TRUE){
+      new_survey$Gender[i]<- 'O'
+    }
+  }
+
+  # Check if we missed any genders
+  new_survey[with(new_survey, grepl("[^FMO]$", Gender)),"Gender"]
+
+  col_names <- c("work_interfere_effective", "work_interfere_not_effective")
+
+  # Combine the interfere levels
+  interfere_levels = c("Not applicable to me", "Never", "Rarely", "Sometimes", "Often")
+  a <- as.numeric(ordered(survey$work_interfere_effective, interfere_levels))
+  b <- as.numeric(ordered(survey$work_interfere_not_effective, interfere_levels))
+
+  for(i in 1:nrow(survey)) {
+    new_survey$work_interfere[i] <- ceiling(mean(c(a[i], b[i])))
+  }
+
+  new_survey$work_interfere <- sapply(new_survey$work_interfere, function(x) 
+    switch(x, 
+      "Not applicable to me", 
+      "Never", 
+      "Rarely", 
+      "Sometimes", 
+      "Often"
+    )
+  )
+  
+  new_survey$tech_company <- sapply(new_survey$tech_company, function(x) 
+    if(is.na(x))
+      return(x)
+    else if(x == 0)
+      return("No")
+    else if(x == 1)
+      return("Yes")
+  )
+  
+  write.csv(new_survey, "C:/Users/Mayank Phadke/Documents/College/DPA/HealthAnalysis/new_survey2016.csv", row.names = FALSE)
+  return(new_survey)
+}
 
 dataVisualize_geomBar<- function(data,aesValue,fillValue,angleValue,vjustValue,titleValue){
     g2 <- ggplot(data, aes(aesValue))
@@ -63,6 +187,31 @@ dataVisualize_geomBar<- function(data,aesValue,fillValue,angleValue,vjustValue,t
     scale_fill_manual(values=c("#3780c4","#c94426","#68a819"))+
     ylab("")+
     labs(fill='Legend')
+}
+
+dataVisualize_geomBar2016 <- function(data,aesValue,fillValue,angleValue,vjustValue,titleValue){
+    g2 <- ggplot(data, aes(aesValue))
+    g2 + geom_bar(aes(fill=fillValue),width = 0.5) + 
+    theme(axis.text.x = element_text(angle=angleValue, vjust=vjustValue)) + 
+    labs(title=titleValue)+
+    scale_fill_manual(
+      labels = c("Empty Values", "Maybe", "No", "Yes"), 
+      values=c("#5b137d", "#c94426", "#3780c4", "#68a819"))+
+    ylab("")+
+    labs(fill='Legend')
+}
+
+dataVisualize_geomBar2016_1 <- function(data,aesValue,fillValue,angleValue,vjustValue,titleValue){
+    g2 <- ggplot(data, aes(aesValue))
+    g2 + geom_bar(aes(fill=fillValue),width = 0.5) + 
+    theme(axis.text.x = element_text(angle=angleValue, vjust=vjustValue)) + 
+    labs(title=titleValue)+
+    scale_fill_manual(
+      labels = c("Empty Values", "Maybe", "No", "Not Eligible", "Yes"),
+      values=c("#5b137d", "#3780c4", "#c94426", "#faaa2a","#68a819"))+
+    ylab("")+
+    labs(fill='Legend')+
+    theme(axis.text.x = element_blank(), axis.ticks.x = element_blank())
 }
 
 dataVisualize_barPlot<- function(data){
@@ -206,4 +355,35 @@ lg_error = calError(lg_cf)
 
 print(paste("Error for Logisticregression :",lg_error))
 
+# 2016 Data
+new_survey2016 = cleanDataSet2016()
+levels(factor(new_survey2016$coworkers))
+#Analysis
+dataVisualize_geomBar2016(new_survey2016,new_survey2016$Age,new_survey2016$supervisor,45,0.5,"Comfortable discussing issues with Supervisors according to age")
+dataVisualize_geomBar2016(new_survey2016,new_survey2016$Age,new_survey2016$coworkers,65,0.6,"Comfortable discussing issues with coworkers according to age")
+dataVisualize_geomBar2016_1(new_survey2016,new_survey2016$benefits,new_survey2016$benefits,45,0.5,"Does the employer provide mental health benefits")
+dataVisualize_barPlot(new_survey2016)
+dataVisualize_barPlot1(new_survey2016)
+new_survey2016 = whichPartOfWorldIsMostAffected()
+#USA is most affected.
+
+
+new_survey2016= reorderingData(new_survey2016)
+listdata = divideData(new_survey2016)
+print(paste("Data is divided as Train and Test :",nrow(listdata$train),",",nrow(listdata$test)))
+
+# Logistic Regression
+
+lg_value = logReg(listdata$train)
+lg_predictValue = predictmethod(lg_value, listdata$test,"response")
+
+# Save the predicted labels using 0.5 as a threshold
+lg_test <-  listdata$test %>%
+  mutate(predTreatment=as.factor(ifelse(lg_predictValue<=0.5, "No", "Yes")))
+
+lg_cf = confusionMatrix(lg_test$predTreatment,lg_test$treatment)
+
+lg_error = calError(lg_cf)
+
+print(paste("Error for Logisticregression :",lg_error))
 
